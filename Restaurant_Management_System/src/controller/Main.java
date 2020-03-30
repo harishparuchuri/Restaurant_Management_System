9abfc75b366c13d6444075b807d9145924874e24
harishparuchuri <harishparuchuri007@gmail.com>
2020-03-30 20:24:12 +0530

    Project created

---
+++ Restaurant_Management_System/src/controller/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * @author haris
+ *
+ */
+import java.util.*;
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		
+
+	}
+
+}
